f4309a7b278691ddc624517a22e1431f2a1b52c8
Mohamed Anwar <mhmdwaelanwr@gmail.com>
2026-08-09 05:45:35 +0300

    feat(frontend): extend Figma store icon set for commerce core

--- elitedom-store/frontend/src/components/store/StoreIcon.tsx
+++ elitedom-store/frontend/src/components/store/StoreIcon.tsx
@@ -1,33 +1,51 @@
 import accountIcon from "@/assets/icons/account.svg";
 import arrowIcon from "@/assets/icons/arrow.svg";
 import cartIcon from "@/assets/icons/cart.svg";
+import compareIcon from "@/assets/icons/compare.svg";
 import deliveryIcon from "@/assets/icons/delivery.svg";
+import filterIcon from "@/assets/icons/filter.svg";
 import menuIcon from "@/assets/icons/menu.svg";
+import minusIcon from "@/assets/icons/minus.svg";
 import packageIcon from "@/assets/icons/package.svg";
 import paymentIcon from "@/assets/icons/payment.svg";
+import plusIcon from "@/assets/icons/plus.svg";
+import returnsIcon from "@/assets/icons/returns.svg";
 import searchIcon from "@/assets/icons/search.svg";
+import sortIcon from "@/assets/icons/sort.svg";
 import warrantyIcon from "@/assets/icons/warranty.svg";
 
 export type StoreIconName =
   | "account"
   | "arrow"
   | "cart"
+  | "compare"
   | "delivery"
+  | "filter"
   | "menu"
+  | "minus"
   | "package"
   | "payment"
+  | "plus"
+  | "returns"
   | "search"
+  | "sort"
   | "warranty";
 
 const iconByName: Record<StoreIconName, string> = {
   account: accountIcon,
   arrow: arrowIcon,
   cart: cartIcon,
+  compare: compareIcon,
   delivery: deliveryIcon,
+  filter: filterIcon,
   menu: menuIcon,
+  minus: minusIcon,
   package: packageIcon,
   payment: paymentIcon,
+  plus: plusIcon,
+  returns: returnsIcon,
   search: searchIcon,
+  sort: sortIcon,
   warranty: warrantyIcon,
 };
 
